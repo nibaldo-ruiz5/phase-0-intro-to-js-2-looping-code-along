b61492c6096957e3cbddfb7b025ad9ffe8a4f0f6
Nibaldo Ruiz <nibaldo78@hotmail.com>
2023-03-14 13:34:53 -0400

    Replace me with a commit message

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const names =["carlos", "javier", "juan"]
+
+
+function writeCards(names, value) {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+  }
+  return messages;
+}
+
+console.log(writeCards(names, "cake"));
+
+function countDown(number){
+    while(number >=0) {
+        console.log(number)
+        number --
+    }
+}
